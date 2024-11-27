097fe37c6c3eaacf8e6ff888a2608031b3a3e26c
RazboiInformational <razboiinformational322@gmail.com>
2024-11-27 09:54:54 +0200

    feat: home with coming soon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
 export default function Home() {
-	return <h1>Home</h1>;
+	return (
+		<div className='px-4 sm:px-6 md:px-8'>
+			<div className='relative max-w-5xl mx-auto pt-20 sm:pt-24 lg:pt-32'>
+				<h1 className='text-slate-900 font-extrabold text-4xl sm:text-5xl lg:text-6xl tracking-tight md:text-center dark:text-white'>
+					Coming soon
+				</h1>
+			</div>
+		</div>
+	);
 }
